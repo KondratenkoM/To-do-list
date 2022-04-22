f9f7757334cd083a1b467b8bd4c12798053af881
Maxim <m0536k@gmail.com>
2022-04-22 12:20:48 +0300

    Initial Commit

---
+++ To do list/To_do_listApp.swift
@@ -0,0 +1,20 @@
+//
+//  To_do_listApp.swift
+//  To do list
+//
+//  Created by apple on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
